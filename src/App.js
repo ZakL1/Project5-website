22def2db6a253c27e44c8a96ea724407e55b2009
ZakL1 <zakerylyel32@gmail.com>
2025-04-12 18:02:14 +0100

    create sign in form and functionality for signing in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import NavBar from'./components/NavBar.js';
+import { AuthProvider } from "./contexts/AuthContext";
+import NavBar from './components/NavBar.js';
 /*import Post from './pages/posts/Post.js';*/
 import styles from './App.module.css';
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
-  return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container>
-        <Routes>
-          <Route path="/" element={<h1>Home page</h1>} />
-          <Route path="/signin" element={<h1>Sign in</h1>} />
-          <Route path="/signup" element={<SignUpForm />} />
-          <Route path="*" element={<p>Page not found!</p>} />
-        </Routes>
-      </Container>
-    </div>
-  );
+    return (
+        <AuthProvider>
+            <div className={styles.App}>
+                <NavBar/>
+                <Container>
+                    <Routes>
+                        <Route path="/" element={<h1> Home page </h1>} />
+                        <Route path="/signin" element={< SignInForm />}/>
+                        <Route path="/signup" element={< SignUpForm />}/>
+                        <Route path="*" element={<p> Page not found !</p>}/>
+                    </Routes>
+                </Container>
+            </div>
+        </AuthProvider>
+    );
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,21 +3,36 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+import Button from 'react-bootstrap/Button';
 
 function NavBar() {
+  const { currentUser, handleLogout } = useAuth();
+
   return (
     <Navbar expand="lg" className={styles.NavBar}>
       <Container>
-        <Navbar.Brand href="#home" className={styles.NavBarBrand}>Website Name</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/" className={styles.NavBarBrand}>Website Name</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/" className={styles.NavLink}>Home</Nav.Link>
-            <Nav.Link as={NavLink} to="/profile" className={styles.NavLink}>Profile</Nav.Link>
+          <Nav.Link as={NavLink} to="/" className={styles.NavLink}>Home</Nav.Link>
+            {currentUser && (
+              <Nav.Link as={NavLink} to="/profile" className={styles.NavLink}>Profile</Nav.Link>
+            )}
           </Nav>
           <Nav>
-            <Nav.Link as={NavLink} to="/signin" className={styles.NavLink}>Sign in</Nav.Link>
-            <Nav.Link as={NavLink} to="/signup" className={styles.NavLink}>Sign up</Nav.Link>
+          {currentUser ? (
+              <>
+                <span className={`me-2 ${styles.NavLink}`}>Hi, {currentUser.username}</span>
+                <Button variant="outline-light" onClick={handleLogout}>Logout</Button>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={NavLink} to="/signin" className={styles.NavLink}>Sign in</Nav.Link>
+                <Nav.Link as={NavLink} to="/signup" className={styles.NavLink}>Sign up</Nav.Link>
+              </>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>

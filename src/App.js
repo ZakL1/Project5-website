af0250dc2c89843ddbb6746bd2213f256f58ced9
ZakL1 <zakerylyel32@gmail.com>
2025-04-12 12:45:44 +0100

    created sign up field, added event listeners

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import NavBar from'./components/NavBar.js';
+/*import Post from './pages/posts/Post.js';*/
 import styles from './App.module.css';
+import { Route, Routes } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="/signin" element={<h1>Sign in</h1>} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="*" element={<p>Page not found!</p>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from 'react-router-dom';
 
-function CollapsibleExample() {
+function NavBar() {
   return (
     <Navbar expand="lg" className={styles.NavBar}>
       <Container>
@@ -11,12 +12,12 @@ function CollapsibleExample() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={styles.NavLink}>Home</Nav.Link>
-            <Nav.Link href="#profile" className={styles.NavLink}>Profile</Nav.Link>
+            <Nav.Link as={NavLink} to="/" className={styles.NavLink}>Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/profile" className={styles.NavLink}>Profile</Nav.Link>
           </Nav>
           <Nav>
-          <Nav.Link href="#login" className={styles.NavLink}>Log in</Nav.Link>
-          <Nav.Link href="#logout" className={styles.NavLink}>Log out</Nav.Link>
+            <Nav.Link as={NavLink} to="/signin" className={styles.NavLink}>Sign in</Nav.Link>
+            <Nav.Link as={NavLink} to="/signup" className={styles.NavLink}>Sign up</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -24,4 +25,4 @@ function CollapsibleExample() {
   );
 }
 
-export default CollapsibleExample;
+export default NavBar;
